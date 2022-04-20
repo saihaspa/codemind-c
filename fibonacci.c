85d1521a8a0551161c4f6625f8aac0fe2796b793
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-20 18:09:03 +0530

    #include<stdio.h> int main() {     int a,b,c,num,temp,i;     scanf("%d",&num);     a=0;b=1;     printf("%d %d ",a,b);     for(i=0;i<num-2;i++)     {         c=a+b;         printf("%d ",c);         a=b;         b=c;
        }
        return 0;
    }

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a=0,b=1,cnt=2,c;
-    scanf("%d",&n);
+    int a,b,c,num,temp,i;
+    scanf("%d",&num);
+    a=0;b=1;
     printf("%d %d ",a,b);
-    for(i=0;i<=n;i++)
+    for(i=0;i<num-2;i++)
     {
         c=a+b;
         printf("%d ",c);
-        cnt++;
         a=b;
         b=c;
-        if(cnt==n)
-        break;
+        
     }
     return 0;
 }
