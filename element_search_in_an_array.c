8dc19bd8f54d2cff6daefe053f85061440003edd
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-21 18:05:49 +0530

    #include<stdio.h> int main() {     int n,k,arr[100],i,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]==k)         {             flag=1;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     }     return 0;
    }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,arr[100],i,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==k)
+        {
+            flag=1;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+    
+}
