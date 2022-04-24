a691ac8f2ae4d19f1494f0bdb6013d7344550364
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-24 12:39:09 +0530

    #include<stdio.h> int main() {     int n,arr[100],b[100],c[100],i,j=0,k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     }     return 0; }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],b[100],c[100],i,j=0,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    return 0;
+}
