57d82d3577cbdac580efe8c6a9ab14fb1c2072e1
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-24 11:23:33 +0530

    #include<stdio.h> int main() {     int l,r,k,count=0;     scanf("%d %d %d",&l,&r,&k);     while(l<=r)     {         if(l%k==0)         {             count++;         }         l++;     }     printf("%d",count);     return 0; }

---
+++ Count_Divisors.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int l,r,k,count=0;
+    scanf("%d %d %d",&l,&r,&k);
+    while(l<=r)
+    {
+        if(l%k==0)
+        {
+            count++;
+        }
+        l++;
+    }
+    printf("%d",count);
+    return 0;
+}
