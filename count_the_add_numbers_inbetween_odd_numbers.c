08a937474e54274176406cdd12d6490b1fa3ba78
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-25 19:26:21 +0530

    #include<stdio.h> int main() {     int n,arr[100],count=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0 && arr[i-1]%2!=0 && arr[i+1]%2!=0)         {             count++;         }     }     printf("%d",count);     return 0; }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],count=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0 && arr[i-1]%2!=0 && arr[i+1]%2!=0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
