3f6b7f424b5e6eed2434f35e49ada8c123058b66
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-04-28 21:33:52 +0530

    #include<stdio.h> int main() {     int n,k,arr[100],i,c=0;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k==0)         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,arr[100],i,c=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
