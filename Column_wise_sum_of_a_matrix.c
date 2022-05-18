4ba379b021ee2e9312bf96353068aa56876a8a5c
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-05-18 18:05:52 +0530

    #include<stdio.h> int main() {     int n,r,i,j,a[100][100],s=0;     scanf("%d%d",&n,&r);     for(i=0;i<n;i++)     {         for(j=0;j<r;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<r;j++)     {s=0;         for(i=0;i<n;i++)         {             s=s+a[i][j];         }         printf("%d ",s);     }     return 0; }

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,i,j,a[100][100],s=0;
+    scanf("%d%d",&n,&r);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<r;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(j=0;j<r;j++)
+    {s=0;
+        for(i=0;i<n;i++)
+        {
+            s=s+a[i][j];
+        }
+        printf("%d ",s);
+    }
+    return 0;
+}
