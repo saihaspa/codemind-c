e89da9585bde60bec5841582adb3c02145d911e6
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-13 07:04:15 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c=0,max=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(a[i]==a[j])                 {                     c++;                     break;                 }             }         }         if(c==0)         {             if(max<a[i])             {                 max=a[i];             }         }
        }
        if(max==0)
        {
            printf("-1");
        }
        else
        {
            printf("%d",max);
        }
    }

---
+++ UNIQUE_MAXIMUM_NUMBER.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],c=0,max=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                    break;
+                }
+            }
+        }
+        if(c==0)
+        {
+            if(max<a[i])
+            {
+                max=a[i];
+            }
+        }
+       
+    }
+    if(max==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",max);
+    }
+}
