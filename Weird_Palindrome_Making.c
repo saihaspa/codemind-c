c648cb4d42869ce2692120b62e85fc1b90808053
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-13 06:18:58 +0530

    #include<stdio.h> int main() {     int x,n,i,t;     scanf("%d",&t);     while(t--)     {         int c=0;         scanf("%d",&n);         for(i=0;i<n;i++)         {             scanf("%d",&x);             if(x%2==1)             {                 c++;             }         }         if(c%2==1)         {             c--;         }         printf("%d ",c/2);     } }

---
+++ Weird_Palindrome_Making.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int x,n,i,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int c=0;
+        scanf("%d",&n);
+        for(i=0;i<n;i++) 
+        {
+            scanf("%d",&x);
+            if(x%2==1)
+            {
+                c++;
+            }
+        }
+        if(c%2==1)
+        {
+            c--;
+        }
+        printf("%d
+",c/2);
+    }
+}
