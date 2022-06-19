853ea52899b48d2137eb30f04722bd7908e545dd
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-19 10:32:07 +0530

    #include<stdio.h> int main() {     int n,m,a[100],b[100],k=0,cnt=0,i,j,x,c[100];     scanf("%d %d",&n,&m);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<m;i++)     {         scanf("%d",&b[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(a[i]==b[j])             {                 cnt=0;                 for(x=0;x<k;x++)                 {                     if(a[i]==c[x])                     {                         cnt++;                     }                 }                 if(cnt==0)                 {                     c[k]=a[i];                     k++;                 }             }         }     }     for(i=0;i<k;i++)     {         printf("%d ",c[i]);     } }

---
+++ Common_elements.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,a[100],b[100],k=0,cnt=0,i,j,x,c[100];
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(a[i]==b[j])
+            {
+                cnt=0;
+                for(x=0;x<k;x++)
+                {
+                    if(a[i]==c[x])
+                    {
+                        cnt++;
+                    }
+                }
+                if(cnt==0)
+                {
+                    c[k]=a[i];
+                    k++;
+                }
+            }
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",c[i]);
+    }
+}
