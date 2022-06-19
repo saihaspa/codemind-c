21cf4103474891ab651644815443c64aaa1174b3
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-06-19 09:35:27 +0530

    #include<stdio.h> int main() {     int n,a[100],i,c=0,k,j,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {c=1;         for(j=0;j<n;j++)         {             if(a[i]==a[j] && i!=j && a[i]>=0)             {                 c++;                 a[j]=-1;             }         }         if(c==k)         {             s++;             printf("%d ",a[i]);         }     }     if(s==0)     {         printf("-1");     } }

---
+++ N_times_repeated_elements.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,c=0,k,j,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {c=1;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j] && i!=j && a[i]>=0)
+            {
+                c++;
+                a[j]=-1;
+            }
+        }
+        if(c==k)
+        {
+            s++;
+            printf("%d ",a[i]);
+        }
+    }
+    if(s==0)
+    {
+        printf("-1");
+    }
+}
