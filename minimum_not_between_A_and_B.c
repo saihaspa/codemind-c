73e15845418b0fad56c69b0b407d791d82413d66
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-03 14:56:47 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],m,k,c=0,min=9999;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&m,&k);     for(i=0;i<n;i++)     {         if(arr[i]<m || arr[i]>k)         {             c++;             if(arr[i]<min)             {                 min=arr[i];             }         }         else         {             continue;         }     }     if(c==0)     {         printf("-1");     }     else     {         printf("%d",min);     } }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],m,k,c=0,min=9999;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&m,&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<m || arr[i]>k)
+        {
+            c++;
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",min);
+    }
+}
