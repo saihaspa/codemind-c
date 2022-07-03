08b964010cbe87d852cc52d32a650d87e2d23e51
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-07-03 14:49:17 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],m,k,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&m,&k);     for(i=0;i<n;i++)     {         if(arr[i]<m || arr[i]>k)         {             c++;             printf("%d ",arr[i]);         }         else         {             continue;         }     }     if(c==0)     {         printf("-1");     }     return 0; }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],m,k,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&m,&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<m || arr[i]>k)
+        {
+            c++;
+            printf("%d ",arr[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    return 0;
+}
