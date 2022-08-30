4df8229f0486af18df443c7f8ab9a3884537d745
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-08-30 11:52:35 +0530

    #include<stdio.h> int main() {     int n,i,a[100],j,c,max;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]==1)         {             c=1;             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }                 else                 {                     break;                 }             }             if(c>max)             {                 max=c;             }         }     }     printf("%d",max); }

---
+++ Max_Consecutive_Ones.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],j,c,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==1)
+        {
+            c=1;
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(c>max)
+            {
+                max=c;
+            }
+        }
+    }
+    printf("%d",max);
+}
