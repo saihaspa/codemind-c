40c913ab18158ba431cd0300229a02c77d9f2902
saihaspa <97425320+saihaspa@users.noreply.github.com>
2022-08-31 16:49:24 +0530

    #include<stdio.h> int main() {     int n,a[100],i,max=0,temp,j,k;     scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
           for(i=0;i<n;i++)
        {
            for(j=i+1;j<n;j++)
            {
                if(a[i]==a[j])   
                {
                    for(k=j;k<n;k++)
                    {
                        a[k]=a[k+1];
                    }
                    n--;
                    j--;
                }            
            }
        }
        if(n<3)
        {
            printf("%d",a[n-1]);
        }
        else
        {
            for(i=0;i<n;i++)
            {
                for(j=i+1;j<n;j++)
                {
                    if(a[i]==a[j])   
                    continue;
                    if(a[i]>a[j])
                    {
                        temp=a[i];
                        a[i]=a[j];
                        a[j]=temp;
                    }
                    else
                    continue;
                }
            }
            printf("%d",a[n-3]);
        }
    }

---
+++ _Third_Maximum_Number.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,max=0,temp,j,k;
+    scanf("%d",&n);
+ 
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+       for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])   
+            {
+                for(k=j;k<n;k++)
+                {
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }            
+        }
+    }
+    if(n<3)
+    {
+        printf("%d",a[n-1]);
+    }
+    else
+    {
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])   
+                continue;
+                if(a[i]>a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+                else
+                continue;
+            }
+        }
+        printf("%d",a[n-3]);
+    }
+}
